9cfbb60d16627a87f416be686df9c4b5f07ae825
Claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2022-07-24 15:36:35 -0300

    cap 6

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  //template: "<p>Aqui es inline empleado</p>",
+  // template: `<p>
+  //     Aqui es inline empleado
+  //   </p>`,
   styleUrls: ['./empleado.component.css']
   //styles:["p{background-color:red}"]
 })
 export class EmpleadoComponent implements OnInit {
+
+  nombre = "Andres";
+  apellido = "Molina";
+  edad=32;
+  empresa = "net";
+
+  esMayor(num: number){return num >= 18 ? "es mayor": "es menor"  }
 
   constructor() { }
 
